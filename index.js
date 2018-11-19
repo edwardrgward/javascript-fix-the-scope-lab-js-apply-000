f1477fd08f772520db7746d6920cb567f4ecde27
kooky-installation-6445 <edwardrgward@gmail.com>
2018-11-19 15:33:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
-var animal = 'dog'
+var animal = 'dog';
 
 function myAnimal() {
-  return animal
+  return animal;
 }
 
 function yourAnimal(){
+<<<<<<< HEAD
   var animal = 'cat'
+=======
+  var animal = 'cat';
+>>>>>>> 31f38e1c5943cb18c364c2cadd8f6a3f2575c9e0
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return animal;
 }
 
 function add2(n) {
+<<<<<<< HEAD
 const two = 2  
 return n + two
+=======
+const two = 2  ;
+return n + two;
+>>>>>>> 31f38e1c5943cb18c364c2cadd8f6a3f2575c9e0
   // Feel free to move things around!
   
 }
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
-  }
-}
+    return "FUNKY!";
+  };
+};
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
+<<<<<<< HEAD
 var theFunk = funkyFunction()()
 
 theFunk
+=======
+var theFunk = funkyFunction()
+var theFunk();
+>>>>>>> 31f38e1c5943cb18c364c2cadd8f6a3f2575c9e0
